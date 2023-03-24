17c4b52890e8b2857aeb8e0c8df7ee3a73a11ee3
Bruno Leão <leaodebrito@icloud.com>
2023-03-23 21:14:29 -0300

    atualização

--- xml_export.py
+++ xml_export.py
@@ -1,3 +1,11 @@
+'''
+Data: 23/03/2023
+Autor: Bruno Brito
+
+Objetivo: junção das classes read_export_xml.py e read_folder.py para criação
+dos arquivos excel na pasta onde estão os arquivos xml originais
+'''
+
 import read_folder as rf
 import read_export_xml as rex
 
@@ -10,7 +18,7 @@ files_path = files.get_path()
 
 #leitura de cada um dos arquivos xml e exportacao para excel
 for i in files_path:
-    xml = rex.Read_export_xml(i, i)
+    xml = rex.Read_export_xml_toExcel(i, i)
     dict_xml = xml.read_xml()
     xml.export_excel(dict_xml)
 
